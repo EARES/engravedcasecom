999208b30e0dc3d060316ad0286078e861b4d5b4
Emre Akbaki <info@emreakbaki.com>
2025-05-21 14:08:12 +0300

    refactor: clean up package.json and remove unused AOS plugin
    - Simplified package.json by removing unnecessary dependencies related to AOS and vue-masonry.
    - Deleted the AOS plugin file as it was no longer in use.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["bootstrap/dist/css/bootstrap.min.css", "@/assets/scss/style.scss", "swiper/css", "swiper/css/effect-fade", "swiper/css/pagination"],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@import "./assets/scss/default/_variables.scss";'
-                },
-            },
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    "@/assets/scss/style.scss",
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./assets/scss/default/_variables.scss";',
         },
+      },
     },
-    nitro: {
+  },
+   nitro: {
         devServer: {
           watch: ['./src']
         }
     },
-    plugins: ["@/plugins/aos"]
-})
+});
