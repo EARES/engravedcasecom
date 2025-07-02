4a6a1c4ef57eb951df7a5fb9d5fa2974208d5d73
Emre Akbaki <info@emreakbaki.com>
2025-07-02 19:42:35 +0300

    feat: add Facebook Pixel tracking scripts and meta tags for improved analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,10 +17,30 @@ export default defineNuxtConfig({
     head: {
       title: "Make your phone truly yours",
       meta: [
+        { name: "facebook-domain-verification", content: "u9zbbj2qvv18y935nuhb8x4zjs7m76" },
         { name: "msapplication-TileColor", content: "#2b5797" },
         { name: "application-name", content: "Engravedcase" },
         { name: "apple-mobile-web-app-title", content: "Engravedcase" },
         { name: "theme-color", content: "#ffffff" },
+      ],
+      script: [
+        {
+          innerHTML: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1311431000988144');
+fbq('track', 'PageView');`
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1311431000988144&ev=PageView&noscript=1" />`
+        }
       ],
       link: [
         {
